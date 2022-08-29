f622a90b4523eb939867300cd62aa7f8a8ec8592
santidelgadoma <60111178+santidelgadoma@users.noreply.github.com>
2022-08-29 13:39:57 -0500

    Update P3A3-ControlServo.ino

--- Practica03/P3A3-ControlServo/P3A3-ControlServo.ino
+++ Practica03/P3A3-ControlServo/P3A3-ControlServo.ino
@@ -1,23 +1,23 @@
-#include <Servo.h>
+#include <Servo.h> // incluimos la biblioteca que corresponde al servo motor
 
-#define pot A0
+#define pot A0 // definir la entrada pot para recibir la señal analógica
 
 
-float value = 0;
-int angulo;
+float value = 0; // inicalizamos la variable value para guardar la lectura analógica
+int angulo; // inicializamos la variable ángulo para calcular los movimientos del servo motor
 
-Servo servo1;
+Servo servo1; // instanciamos un servo
 
 void setup() {
-  servo1.attach(13);
-  Serial.begin(9600);
+  servo1.attach(13); // anclamos el servo a el puerto 13
+  Serial.begin(9600); // estabblecemos la velocidad de la comunicación entre la computadora y el microcontrolador
 }
 
 void loop() {
 
-  value = analogRead(pot);
-  angulo = map(value,0,1023,0,180);
-  servo1.write(angulo);
-  delay(100);
+  value = analogRead(pot); // guardamos las lecturas analogicas en la variable value
+  angulo = map(value,0,1023,0,180); // definimos los parametros con los que se medira el angulo para los movimientos
+  servo1.write(angulo); // pasamos el valor calculado en angulo para mover el servo motor dependiendo de la lectura analógica
+  delay(100); // aplicamos un delay para que no se mueva todo el tiempo el servo motor
 
 }
