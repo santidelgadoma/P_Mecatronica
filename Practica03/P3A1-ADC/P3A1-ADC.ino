7ba9b561098f5e5fbe866a0009d5990c6e982065
santidelgadoma <60111178+santidelgadoma@users.noreply.github.com>
2022-08-29 13:14:47 -0500

    Update P3A1-ADC.ino

--- Practica03/P3A1-ADC/P3A1-ADC.ino
+++ Practica03/P3A1-ADC/P3A1-ADC.ino
@@ -1,28 +1,28 @@
-#define pot A0
-#define led 13
-float value = 0;
-float voltaje;
+#define pot A0   // definimos el componente pot como el lector analógico
+#define led 13.  // definimos la salide led en el puerto 13
+float value = 0; // definimos un valor float llamado value para guardar la lectura analógica
+float voltaje;   // definimos un valor float llamado voltaje que usaremos para imprimir la variacion del voltaje
 void setup() {
-  // No definimos pinMode porque directamennte leeremos una entrada analógica
-  pinMode(led, OUTPUT);
-  Serial.begin(9600);
+
+  pinMode(led, OUTPUT); // definimos el pinMode
+  Serial.begin(9600);  // indicamos la velocidad de comunicacion entre el microcontrolador y la computadora
 
 }
 
 void loop() {
-  value = analogRead(pot) ;
-  voltaje = (value/1023*5);
-  Serial.print("Voltaje: ");
+  value = analogRead(pot) ; // usamos la función analogRead para medir la información analógica
+  voltaje = (value/1023*5); // calculamos el voltaje usando una regla de tres
+  Serial.print("Voltaje: "); //imprimimos en una terminal la lectura obtenida
   Serial.print(voltaje);
   Serial.println("V");
-  delay(100);
+  delay(100); // programamos un retardo de 1 segundo por lectura
 
-  if (voltaje > 3) {
-    digitalWrite(led, HIGH);
+  if (voltaje > 3) {.  // con este enunciado dividimos los voltajes entre menores a 3 y mayores a 3
+    digitalWrite(led, HIGH); // si la lectura de voltaje es mayor a 3 entonces se prende el led
   }
 
   else{
-    digitalWrite(led, LOW);
+    digitalWrite(led, LOW); // si la lectura de voltaje es menor a 3 entonces se apaga el led
   }
 
 }
