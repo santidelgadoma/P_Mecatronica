53611dcba03a272ba50f003323860d9e9ebce3c5
santidelgadoma <60111178+santidelgadoma@users.noreply.github.com>
2022-08-29 13:32:35 -0500

    Update P3A2-PWM.ino

--- Practica03/P3A2-PWM/P3A2-PWM.ino
+++ Practica03/P3A2-PWM/P3A2-PWM.ino
@@ -1,26 +1,26 @@
-#define pot A0
-#define led 13
-float value = 0;
-float brillo;
+#define pot A0  //definimos pot como la entrada de lectura analógica
+#define led 13  //definimos un led como salida del puerto 13
+float value = 0; //inicializamos una variable para guardar la lectura analógica
+float brillo; // inicializamos una variable que usaremos para guardar un cálculo
 
-int pwm = 0;
+int pwm = 0; // inicializamos una variable en la que guardaremos un cálculo
 
 void setup() {
   // No definimos pinMode porque directamennte leeremos una entrada analógica
   pinMode(led, OUTPUT);
-  Serial.begin(9600);
+  Serial.begin(9600); // establecemos la velocidad de la comunicación entre computadora y microcontrolador
 
 }
 
 void loop() {
-  value = analogRead(pot) ;
-  pwm = value/4;
+  value = analogRead(pot) ; // leemos la entrada analógica
+  pwm = value/4; // sacamos el valor pwm
   
-  analogWrite(led, pwm);
+  analogWrite(led, pwm); //asignamos lo obtenido como parametro de la salida led
   
-  brillo = value/1023*100;
-  Serial.print("Brillo: ");
+  brillo = value/1023*100; // convertimos a voltaje para establecer el brillo 
+  Serial.print("Brillo: "); // imprimimos lo calculado en la terminal
   Serial.println(brillo);
-  delay(0);
+  delay(0); // Ponemos un delay de 0 para que no se detecte el cambio entre estados del led y fluya
 
 }
