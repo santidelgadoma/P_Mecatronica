9f2a71512fc0e4b002df81a7ec1b73d201c53b6e
santidelgadoma <60111178+santidelgadoma@users.noreply.github.com>
2022-11-30 19:48:30 -0600

    Update P10A2-MaqEdos.ino

--- Practica10/P10A2-MaqEdos/P10A2-MaqEdos.ino
+++ Practica10/P10A2-MaqEdos/P10A2-MaqEdos.ino
@@ -15,14 +15,14 @@ void loop() {
 
   switch(estado){
     case 0:
-      Serial.println("Estado 0");
+      Serial.println("Estado 0");   //Comenzamos en el estado cero y de inmediato cambiamos al estado 1
       estado = 1;
       break;
       
      case 1:
       Serial.println("Estado 1");
       digitalWrite(LED, LOW);
-      if (abs(light_dif) < 30){
+      if (abs(light_dif) < 30){. //En el estado 1 segun la variable light_diff nos regresamos al estado 0 o avazamos al estado 2
         estado = 2;
       }
       else{
@@ -30,7 +30,7 @@ void loop() {
       }
       break;
     case 2:
-      Serial.println("Estado 2");
+      Serial.println("Estado 2"); //En el estado 2 activamos el led durante un segundo y regresamos al estado inicial
       digitalWrite(LED, HIGH);
       delay(1000);
       estado = 0;
